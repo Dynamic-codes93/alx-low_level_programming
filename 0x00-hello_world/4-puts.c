574fb43b169f4f332928f19f5bc4516002dd1832
Dynamic-codes93 <dammyb36@gmail.com>
2022-09-16 05:41:58 -0700

    modified

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ * main - entry point
+ * Description: 'What is programming'
+ * return value - Always 0 (Success)
+ */
 int main(void)
-{ puts("\"Programming is like building a multilingual puzzle");
+{ puts("Programming is like building a multilingual puzzle\n");
 		return (0); }
